86fdcb5c2cef78822231d8aaf325d13a7660fc02
Dean Flood <deanflood1991@gmail.com>
2016-12-10 23:48:50 +0000

    Adding comments, coloured graphs

--- performanceScript.r
+++ performanceScript.r
@@ -1,21 +1,35 @@
+#Sets working directory to location of relevant files
 setwd("S:/Users/Dean's PC/workspace/EAA_CA2")
 
+#reads result.dat into a table variable, taking into account the header of the file
 performanceData <- read.table("results.dat", header=TRUE)
 
+#Time - 10seconds as that is the period the data is captures by bash script
 T <- 10
+
+#Utilisation - Captured by subtracting the CPU Idle time from 100. ie 20% idle means utilisation is 80% (100 - 20 = 80)
 UI <- (100-performanceData$IDLE)
-X0 <- (performanceData$CO/T)
-Di <- (UI / X0)
-R <- (performanceData$N / X0)
+
+#System Throughput - (XO) is acheived from dividing total completed transactions(CO) by the time (T)
+XO <- (performanceData$CO/T)
+
+#Service demand - Utilisation divided by System throughput
+DI <- (UI / XO)
+
+#Response Time - Total users divided by throughput
+R <- (performanceData$N / XO)
+
+#dev.new() opens a new window to display plot. Means all plots are generated at the same time on screen
+dev.new()
+
+#pch sets vertices to be solid bullet points. Colour of the points is determine depending on the value of UI. Green is low, yellow medium, red is high utilisation
+plot(UI, pch=20, main="Utilisation (UI)", xlab="Number of Users (N)", ylab="Utilisation (%)", col=ifelse(UI >= 90, "red3", ifelse(UI <= 30, "green4", "gold3")))
 
 dev.new()
-plot(UI, main="Utilisation (UI)", xlab="Number of Users (N)", ylab="Utilisation (%)")
+plot(DI, pch=20, main="Service Demand (Di)", xlab="Number of Users (N)", ylab="Average time spent obtaining service" , col="dodgerblue4")
 
 dev.new()
-plot(Di, main="Service Demand (Di)", xlab="Number of Users (N)", ylab="Average time spent obtaining service")
+plot(XO, pch=20, main="Throughput (XO)", xlab="Number of Users (N)", ylab="Transactions per second" , col="green4",)
 
 dev.new()
-plot(X0, main="Throughput (X0)", xlab="Number of Users (N)", ylab="Transactions per second")
-
-dev.new()
-plot(R, main="Response Time (R)", xlab="Number of Users (N)", ylab="Seconds")
+plot(R, pch=20, main="Response Time (R)", xlab="Number of Users (N)", ylab="Seconds" , col=ifelse(R >= 0.9, "red3", ifelse(R <= 0.3, "green4", "gold3")))
--- runtest.sh
+++ runtest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "IDLE N CO" >> results.dat
+echo "IDLE N CO" >> results.dat						#Adding header for results file
 
-for i in {1..50}
+for i in {1..50} 									#Loop for 50 simulated users
 do
-    ./loadtest $i &
-    idle=$(mpstat 1 10 | awk 'NR==14{print $12}')
-    pkill loadtest
-    echo $idle " " $i " " $(wc -l synthetic.dat | awk '{print $1}') >> results.dat
+    ./loadtest $i &									#Runs loadtest with current index as number of users. & symbol runs program in background
+    idle=$(mpstat 1 10 | awk 'END{print $NF}')		#Captures the processor load every second for 10 seconds. awk used to capture average of 10 measurements
+    pkill loadtest									#Kills loadtest after results are captured
+    echo $idle " " $i " " $(wc -l synthetic.dat | awk '{print $1}') >> results.dat	#Echos results of load test and N into file. wc -l used to caputre C0 from generaated synthetic.dat
 done
